edfd1553ed946eeff16f30d3bc590407a1166b43
jonathas <jonathas_tc@hotmail.com>
2021-10-27 19:21:05 -0300

    url

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,8 +23,8 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-//	public $baseURL = 'http://localhost:8080/';
-    public $baseURL = 'https://growx-tcc.herokuapp.com/';
+	public $baseURL = 'http://localhost:8080/';
+//    public $baseURL = 'https://growx-tcc.herokuapp.com/';
 
 	/**
 	 * --------------------------------------------------------------------------
